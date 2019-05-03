a38ba2c990787c25e89057b046f74f1c3dfa5b92
Mike Roberts <mikebroberts@hotmail.com>
2019-05-02 18:02:57 -0600

    second commit, cleaned up code a bit and added some functional

--- public/js/script.js
+++ public/js/script.js
@@ -1,39 +1,43 @@
 // jshint esversion: 6
-/* authorization code: BQD3o6M_j01OWlolSdx7muL1OaLXrKzkwd60YKR1KkY5jGfuiaUAs3Bjbe8lX9_H6DfmM4VdFC0g7J3y4Ms0pmsgud43C2k2HWboSYpQg5xn04zarRDI5EsaAgYWb3t8LMtlpPQEEi0Ftz1x_BO6Bp5DNgHOgZ0 */
 
-// curl -X "GET" "https://api.spotify.com/v1/search?q=Muse&type=track%2Cartist&market=US&limit=10&offset=5" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer BQD3o6M_j01OWlolSdx7muL1OaLXrKzkwd60YKR1KkY5jGfuiaUAs3Bjbe8lX9_H6DfmM4VdFC0g7J3y4Ms0pmsgud43C2k2HWboSYpQg5xn04zarRDI5EsaAgYWb3t8LMtlpPQEEi0Ftz1x_BO6Bp5DNgHOgZ0"
-
-/*
-https://accounts.spotify.com/authorize?client_id=5fe01282e94241328a84e7c5cc169164&redirect_uri=http:%2F%2Fexample.com%2Fcallback&scope=user-read-private%20user-read-email&response_type=token&state=123
+/*Curl we were analyzing
+    curl -X "GET" "https://api.spotify.com/v1/search?q=Muse&type=track%2Cartist&market=US&limit=10&offset=5" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer BQD3o6M_j01OWlolSdx7muL1OaLXrKzkwd60YKR1KkY5jGfuiaUAs3Bjbe8lX9_H6DfmM4VdFC0g7J3y4Ms0pmsgud43C2k2HWboSYpQg5xn04zarRDI5EsaAgYWb3t8LMtlpPQEEi0Ftz1x_BO6Bp5DNgHOgZ0"
 */
 
-// console.log(window.location.hash.substr(1));
+// get access token out of url returned from spotify
 const urlParams = new URLSearchParams(window.location.hash.replace('#', '?'));
-
-console.log()
 const access_token = urlParams.get('access_token');
-// console.log(urlParams.getAll());
-// for (let p of urlParams.entries()) {
-//   console.log(p);
-// }
-// console.log(urlParams);
 
-const offset = 5;
+/* don't know if we need this
+  const urlify = function(str) {
+  return str.trim().replace(/\s+/g, '%20');
+};*/
 
-// fetch('url')
-//   .then((res) => res.json())
-//   .then((res) => {
-//     console.log('AUTH RESPONSE', res);
 
-fetch(`https://api.spotify.com/v1/search?q=Muse&type=track%2Cartist&market=US&limit=10&offset=${offset}`, {
-    method: 'GET',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + access_token
-    }
-  }).then((res) => res.json())
-  .then(res => {
-    console.log("RES", res);
-  });
-// });
+$("#search-form").submit(function(event) {
+  event.preventDefault(); //prevent from refreshing
+  let artist = $("#searchedName").val(); // use searched name
+  console.log(artist);
+  fetch(`https://api.spotify.com/v1/search?q=${artist}&type=track%2Cartist&market=US&limit=5&offset=0`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + access_token
+      }
+    }).then((res) => res.json())
+    .then(res => {
+      console.log("RES", res); // show result in console
+
+      // populate images boxes with search result images and names
+      for (let i = 1; i <= 5; i++) {
+        if (res.artists.items[i - 1].images.length === 0) {
+          $('#searchImg' + i).attr('src', '/images/noPic.jpg');
+        } else {
+          $('#searchImg' + i).attr('src', res.artists.items[i - 1].images[1].url);
+          let name_string = res.artists.items[i - 1].name;
+          $('#searchImgName' + i).html(name_string);
+        }
+      }
+    });
+});
